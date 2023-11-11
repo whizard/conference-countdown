b1b00097d6f2f1953bd20c98e214c868940c3a07
Paul MacKay <paul.themac@gmail.com>
2023-11-11 13:38:36 -0700

    cleanup

--- a.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PORT=3000
-pgrep countdown | xargs kill -9 2>/dev/null
-
-./dist/countdown&
-
-firefox "--kiosk" http://localhost:3000/?json=/wdc2023.json&track=A&show_seconds=true
-
